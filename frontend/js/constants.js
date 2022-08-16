ebbc4c357847ddbd97eb1b12d98b940314e7d0b2
tHEmANoWNER <108401210+tHEmANoWNER@users.noreply.github.com>
2022-08-16 13:56:39 +0100

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x2e74746FC67639bB0f5fF2382Ce7C951ffc6c083"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
